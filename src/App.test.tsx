ce41d1f148e26a8eb889508a508b3b4407801ab4
Timothy Li <timothy93li@gmail.com>
2025-03-05 18:38:16 +0000

    testing: set up Vitest testing and github action workflow

---
+++ src/App.test.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+test("renders login page link", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Hi/i); // Adjust this text based on what your app renders
+  expect(linkElement).toBeInTheDocument();
+});
