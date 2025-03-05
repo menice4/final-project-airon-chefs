568ddcd8b857ee76b1142ab636838fd81acad9ca
Sergio <sergeh96@gmail.com>
2025-03-05 16:54:50 +0000

    working client side

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Lobby from "./Pages/QuizLobbyPage/Lobby";
 import QuizPage from "./Pages/QuizPage/QuizPage";
 import EndPage from "./Pages/EndPage/EndPage";
-import HomePage from "./Pages/Homepage/HomePage";
+import HomeScreen from "./Pages/HomePage/HomeScreen";
 import LoginPage from "./Pages/LoginPage/LoginPage";
 // serge branch
 /* import Lobby from "./Components/Lobbypage/Lobby"; */
@@ -14,7 +14,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<LoginPage />} />
-        <Route path="/home" element={<HomePage />} />
+        <Route path="/home" element={<HomeScreen />} />
         <Route path="/quiz-lobby" element={<Lobby />} />
         <Route path="/quiz" element={<QuizPage />} />
         <Route path="/end" element={<EndPage />} />
