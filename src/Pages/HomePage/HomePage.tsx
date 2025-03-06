5bcd4f3136661fccab5e1b55181baa526c25a2b1
Jackafella1 <jackafella@gmail.com>
2025-03-06 11:34:04 +0000

    login fully functional

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { useContext } from 'react';
-import { AuthContext } from '../../Context/AuthContext';
+import { AuthContext } from '../../context/AuthContext';
 
 const HomePage = () => {
   const authContext = useContext(AuthContext);
