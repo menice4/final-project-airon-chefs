3cb39b19e3f51a7c140d54c1c30d9fba15f7f2f6
Ashton Bailey <95630289+menice4@users.noreply.github.com>
2025-03-14 14:26:13 +0000

    asd

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3000;
 app.use(
   cors({
     origin: [
-      "https://final-project-quiz-mania.vercel.app"
+      "https://final-project-airon-chefs.onrender.com/"
       
     ],
 
